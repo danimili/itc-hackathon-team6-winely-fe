d56aead720441680b145db6dde5bccef31543a04
Amitai Zand <amitai17@gmail.com>
2023-01-25 20:14:35 +0200

    Integrating

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Search from '../components/Search'
-import AllCountries from'../components/AllCountries';
-import FavoriteCountries from'../components/FavoriteCountries';
+import React from "react";
+import Search from "../components/Search";
+import AllCountries from "../components/AllCountries";
+import FavoriteCountries from "../components/FavoriteCountries";
 
 export default function SearchPage() {
-  return (
-    <>
-    <h1>SearchPage</h1>
-    <Search/>
-    <FavoriteCountries/>
-    <AllCountries/>
-
-    </>
-  )
+    return (
+        <>
+            <h1>SearchPage</h1>
+            <Search />
+            <FavoriteCountries />
+            <AllCountries />
+        </>
+    );
 }
