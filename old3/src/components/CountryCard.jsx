437cf9d21489f57161b5260051147a2aa851e06e
tomdagan1995 <tomdagan1995@gmail.com>
2023-01-25 19:44:53 +0200

    Co-authored-by: Yair Rosenschein <yairros@users.noreply.github.com> Co-authored-by: Daniela Milieris <danimili@users.noreply.github.com> Co-authored-by: Alon Kerklies <AlonKerklies@users.noreply.github.com> Co-authored-by: belbcode <belbcode@users.noreply.github.com>

---
+++ old3/src/components/CountryCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+
+export default function CountryCard({countryName, flagUrl}) {
+
+  const navigate = useNavigate();
+  
+  const navigateRecommendation = () => {
+    navigate(`/RecommendationPage/${countryName}`);
+  };
+
+  return (
+ 
+ <Container  >
+
+<Row  
+ className='country-card mb-2  '
+ onClick={navigateRecommendation } > 
+ <Col xs={2}><img className=" country-img m-0" src={flagUrl} alt={countryName}/> </Col>
+ <Col xs={10}  className=' '> <p className="card-countryName    "> {countryName}</p></Col>
+ 
+
+</Row>
+
+</Container>  
+  )
+}
+
+
+
+
+
+
+
+
+
+     {/* <img className=" " src={argentina}  />  */}
+     {/* <img className=" " src={../flagImages/argentina.png}  /> */}
+     {/* <img src="../flagImages/argentina.png"  /> */}
+    {/* <img className=" " src={"./flagImages/argentina.png"} /> */}
+    {/* <img src="../flagImages/argentina.png"  /> */}
+ 
---
+++ old3/src/pages/RecommendationPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RecommendationPage() {
+  return (
+    <div>RecommendationPage</div>
+  )
+}
