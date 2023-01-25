5c3658063a85de505706f7bdb8e879f1d2314322
Amitai Zand <amitai17@gmail.com>
2023-01-25 19:23:50 +0200

    Improved LandingPage CSS

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from "react";
 import { useNavigate, Link } from "react-router-dom";
-import './LandingPage.css'
-import RecommendationPage from './RecommendationPage';
+import "./LandingPage.css";
+import RecommendationPage from "./RecommendationPage";
 
 export default function LandingPage() {
-  const navigate = useNavigate()
+    const navigate = useNavigate();
 
-  const handleStart = async () => {
-    navigate("/Search")
-  }
-  return (
-    <>
-    <body className="body">
-      <div className= "home-text">
-   <h1 className="welcome">Welcome to Winely</h1>
-   <h3 className="text">Your Costumized Wine Recommendation to get to know Wines from Different Cultures </h3>
-   </div>
-   <div>
-   <button className="get-started-btn" onClick={handleStart}>Get Started</button>
-   </div>
-   </body>
-    </>
-  )
+    const handleStart = async () => {
+        navigate("/Search");
+    };
+    return (
+        <body className="body">
+            <h1 className="welcome">Welcome to Winely</h1>
+            <h3 className="text">
+                Your Data Science-Powered Robo-Sommelier Recommending Wines from
+                Different Cultures{" "}
+            </h3>
+            <div>
+                <button className="get-started-btn" onClick={handleStart}>
+                    Get Started
+                </button>
+            </div>
+        </body>
+    );
 }
