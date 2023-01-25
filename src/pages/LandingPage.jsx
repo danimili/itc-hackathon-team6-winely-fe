7beeab7c75096a4801770808570ae3517c6f6d34
danimili <danielamilieris@gmail.com>
2023-01-25 12:59:35 +0200

    pagesFirstPush

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useNavigate, Link } from "react-router-dom";
+import './LandingPage.css'
 
 export default function LandingPage() {
+  const navigate = useNavigate()
+
+  const handleStart = async () => {
+    navigate("/Search")
+  }
   return (
     <>
-   <h1>LandingPage</h1> 
-   <button>Get Started</button>
+   <h1 className="welcome">LandingPage</h1>
+   <div>Welcome + Explanation </div>
+   <button onClick={handleStart}>Get Started</button>
     </>
   )
 }
