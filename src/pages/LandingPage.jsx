032bbc391e708cda08bc579d2ff9c4bb72db53f6
danimili <danielamilieris@gmail.com>
2023-01-25 12:35:07 +0200

    working routes

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <>
+   <h1>LandingPage</h1> 
+   <button>Get Started</button>
+    </>
+  )
+}
---
+++ src/pages/WinePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WinePage() {
+  return (
+    <div>WinePage</div>
+  )
+}
