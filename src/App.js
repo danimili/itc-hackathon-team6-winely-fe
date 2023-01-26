05c6aad3d79175e1bf2e1cb54cdd668c1e53e9bd
danimili <danielamilieris@gmail.com>
2023-01-26 10:48:54 +0200

    wineForm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LandingPage from "./pages/LandingPage";
 import SearchPage from "./pages/SearchPage";
 import WinePage from "./pages/WinePage";
 import RecommendationPage from "./pages/RecommendationPage";
+import WineForm from "./components/WineForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { useState } from "react";
@@ -25,11 +26,9 @@ function App() {
                 <Routes>
                     <Route index element={<LandingPage />} />
                     <Route path="/Search" element={<SearchPage />}></Route>
-                    <Route
-                        path="/RecommendationPage"
-                        element={<RecommendationPage />}
-                    ></Route>
+                    <Route path="/RecommendationPage" element={<RecommendationPage />}></Route>
                     <Route path="/Wines" element={<WinePage />}></Route>
+                    <Route path="/WineForm" element={<WineForm />}></Route>
                 </Routes>
             </AppContext.Provider>
         </>
