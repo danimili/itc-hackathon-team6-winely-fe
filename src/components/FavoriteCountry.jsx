d468bc58116e59a1deb5ccc159eedd82c6f64f4e
tomdagan1995 <tomdagan1995@gmail.com>
2023-01-26 10:48:00 +0200

    10:48

--- src/components/FavoriteCountry.jsx
+++ src/components/FavoriteCountry.jsx
@@ -43,22 +43,5 @@ export default function FavoriteCountry({countryName, flagUrl}) {
 
 
  
-=======
-import React from "react";
-import Row from "react-bootstrap/Row";
-import { useNavigate } from "react-router-dom";
 
-export default function FavoriteCountry({ countryName, flagUrl }) {
-    const navigate = useNavigate();
 
-    const navigateRecommendation = () => {
-        navigate(`/RecommendationPage/${countryName}`);
-    };
-
-    return (
-        <Row className="favorite-country" onClick={navigateRecommendation}>
-            <img className=" fav-country-img" src={flagUrl} alt={countryName} />
-            <p className="favorite-country-name"> {countryName}</p>
-        </Row>
-    );
-}
