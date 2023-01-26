aea9cc807a54c500f1c06f7286b1ee396f7d4965
Amitai Zand <amitai17@gmail.com>
2023-01-26 08:36:18 +0200

    Fixed missing Bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import AppContext from './AppContext';
-import LandingPage from './pages/LandingPage';
-import SearchPage from './pages/SearchPage';
-import WinePage from './pages/WinePage'
-import RecommendationPage from './pages/RecommendationPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AppContext from "./AppContext";
+import LandingPage from "./pages/LandingPage";
+import SearchPage from "./pages/SearchPage";
+import WinePage from "./pages/WinePage";
+import RecommendationPage from "./pages/RecommendationPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { useState } from 'react';
+import { useState } from "react";
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
-  const [wineDetails, setWineDetails] = useState({});
+    const [searchResults, setSearchResults] = useState([]);
+    const [wineDetails, setWineDetails] = useState({});
 
-  
-  return (
-    <>
-    <AppContext.Provider value={{ searchResults, setSearchResults, wineDetails, setWineDetails}}>
-      <Routes>
-        <Route index element={<LandingPage/>}/>
-        <Route path="/Search" element = {<SearchPage/> } ></Route>
-        <Route path="/RecommendationPage" element = {<RecommendationPage/>}></Route>
-        <Route path="/Wines" element = {<WinePage/>}></Route> 
-      </Routes>
-      </AppContext.Provider>
-      
-    </>
-  );
+    return (
+        <>
+            <AppContext.Provider
+                value={{
+                    searchResults,
+                    setSearchResults,
+                    wineDetails,
+                    setWineDetails,
+                }}
+            >
+                <Routes>
+                    <Route index element={<LandingPage />} />
+                    <Route path="/Search" element={<SearchPage />}></Route>
+                    <Route
+                        path="/RecommendationPage"
+                        element={<RecommendationPage />}
+                    ></Route>
+                    <Route path="/Wines" element={<WinePage />}></Route>
+                </Routes>
+            </AppContext.Provider>
+        </>
+    );
 }
 
 export default App;
